6b37dcdbf7c910f5980215bd030a1246663141d4
Minh <smgames2099@gmail.com>
2023-11-19 17:48:59 +0700

    update

--- Backend/admin/ajax/refund_bookings.php
+++ Backend/admin/ajax/refund_bookings.php
@@ -1,8 +1,8 @@
 <?php 
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: *");
 
 require('../inc/db_config.php');
 require('../inc/essentials.php');
--- Backend/ajax/login_register.php
+++ Backend/ajax/login_register.php
@@ -1,8 +1,8 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: *");
 
 require('../admin/inc/db_config.php');
 require('../admin/inc/essentials.php');
