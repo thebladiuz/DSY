1812f2218fa750b20b6f38aec11f223404253eb2
Minh <smgames2099@gmail.com>
2023-11-19 20:50:19 +0700

    Update index.php

--- Backend/admin/ajax/index.php
+++ Backend/admin/ajax/index.php
@@ -1,3 +1,16 @@
+<?php
+// Allow requests from a specific origin
+header('Access-Control-Allow-Origin: http://34.143.182.143:8080');
+
+// Allow additional headers if needed
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+// Set allowed methods (GET, POST, etc.)
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+
+// Allow credentials if needed
+header('Access-Control-Allow-Credentials: true');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
