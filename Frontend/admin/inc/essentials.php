d756e08398643e57f3bfbe2a7ae569772d83eaa5
Minh <smgames2099@gmail.com>
2023-11-20 16:45:41 +0700

    update

--- Frontend/admin/inc/essentials.php
+++ Frontend/admin/inc/essentials.php
@@ -8,61 +8,40 @@ define('ROOMS_IMG_PATH', SITE_URL.'assets/images/rooms/');
 define('USERS_IMG_PATH', SITE_URL.'assets/images/users/');
 
 //backend upload process needs this date_add
-define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/assets/images/');
+define('UPLOAD_IMAGE_PATH', FRONTEND_BASE_URL . 'upload-handler.php');
 define('ABOUT_FOLDER','about/');
 define('CAROUSEL_FOLDER','carousel/');
 define('FACILITIES_FOLDER','facilities/');
 define('ROOM_FOLDER','rooms/');
 define('USERS_FOLDER','users/');
 
-// sendgrid api key
-
-//define('SENDGRID_API_KEY',"...");
-    function adminLogin(){
-        session_start();
-        if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
-            header("location: index.php");
-            echo"<script>
-                window.location.href='index.php';
-            </script>";
-            exit;
-        }
-    }
-    function redirect($url){
+function adminLogin(){
+    session_start();
+    if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
+        header("location: index.php");
         echo"<script>
-        window.location.href='$url';
-            </script>";
-            exit;
-        
+            window.location.href='index.php';
+        </script>";
+        exit;
     }
-    function alert($type,$msg){
-        $bs_class = ($type == "success") ? "alert-success" : "alert-danger";
-        echo <<<alert
-            <div class="alert $bs_class alert-dismissible fade show custom-alert " role="alert">
-                <strong class="me-3">$msg</strong> 
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        alert;
-    }
+}
+function redirect($url){
+    echo"<script>
+    window.location.href='$url';
+        </script>";
+        exit;
 
-    function uploadImage($image, $folder){
-        $valid_mime = ['image/jpeg', 'image/png', 'image/webp', 'image/jpg'];
-        $img_mime = $image['type'];
-        if (!in_array($img_mime, $valid_mime)) {
-            return 'inv_img'; // Invalid image mime or format
-        } else if (($image['size'] / (1024 * 1024)) > 2) {
-            return 'inv_size'; // Invalid size greater than 2
-        }else{
-            $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
-            $rname = 'IMG_'.random_int(11111, 99999).".$ext";
-            $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
-            if(move_uploaded_file($image['tmp_name'], $img_path)){
-                return $rname;
-            }else{
-                return 'upd_failed';
-            }
-        }
-    }
+}
+function alert($type,$msg){
+    $bs_class = ($type == "success") ? "alert-success" : "alert-danger";
+    echo <<<alert
+        <div class="alert $bs_class alert-dismissible fade show custom-alert " role="alert">
+            <strong class="me-3">$msg</strong> 
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    alert;
+}
+
     function deleteImage($image, $folder) {
         $imagePath = UPLOAD_IMAGE_PATH . $folder . $image;
         if (file_exists($imagePath) && unlink($imagePath)) {
---
+++ Frontend/uploadhandler.php
@@ -0,0 +1,20 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+require('admin/inc/essentials.php'); // Adjust the path accordingly
+
+if (isset($_POST['add_image'])) {
+    $img_r = uploadImage($_FILES['picture'], CAROUSEL_FOLDER);
+
+    if ($img_r === 'inv_img') {
+        echo $img_r;
+    } else if ($img_r === 'inv_size') {
+        echo $img_r;
+    } else if ($img_r === 'upd_failed') {
+        echo $img_r;
+    } else {
+        echo 'success';
+    }
+}
+?>
