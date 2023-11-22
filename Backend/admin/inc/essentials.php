65116a39e0a239fda42b6d0ee83f05f7b9555bb1
Minh <smgames2099@gmail.com>
2023-11-22 22:12:18 +0700

    Update essentials.php

--- Backend/admin/inc/essentials.php
+++ Backend/admin/inc/essentials.php
@@ -1,6 +1,6 @@
 <?php
 //frontend purpose data
-define('SITE_URL', 'http://34.143.182.143:8080/');
+define('SITE_URL', 'http://34.143.182.143:8080/'); 
 define('ABOUT_IMG_PATH', SITE_URL.'assets/images/about/');
 define('CAROUSEL_IMG_PATH', SITE_URL.'assets/images/carousel/');
 define('FACILITIES_IMG_PATH', SITE_URL.'assets/images/facilities/');
@@ -8,12 +8,14 @@ define('ROOMS_IMG_PATH', SITE_URL.'assets/images/rooms/');
 define('USERS_IMG_PATH', SITE_URL.'assets/images/users/');
 
 //backend upload process needs this date_add
-define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/assets/images/');
+define('UPLOAD_IMAGE_PATH', SITE_URL.'/assets/images/');
 define('ABOUT_FOLDER','about/');
 define('CAROUSEL_FOLDER','carousel/');
 define('FACILITIES_FOLDER','facilities/');
 define('ROOM_FOLDER','rooms/');
 define('USERS_FOLDER','users/');
+
+$GLOBALS['frontendImagePath'] = 'http://34.143.182.143:8080/assets/images/';
 
     function adminLogin(){
         session_start();
@@ -52,7 +54,7 @@ define('USERS_FOLDER','users/');
         }else{
             $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
             $rname = 'IMG_'.random_int(11111, 99999).".$ext";
-            $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+            $img_path = $GLOBALS['frontendImagePath'].$folder.$rname;
             if(move_uploaded_file($image['tmp_name'], $img_path)){
                 return $rname;
             }else{
@@ -79,7 +81,7 @@ define('USERS_FOLDER','users/');
         }else{
             $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
             $rname = 'IMG_'.random_int(11111, 99999).".$ext";
-            $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+            $img_path = $GLOBALS['frontendImagePath'].$folder.$rname;
             if(move_uploaded_file($image['tmp_name'], $img_path)){
                 return $rname;
             }else{
@@ -98,7 +100,7 @@ define('USERS_FOLDER','users/');
         }else{
             $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
             $rname = 'IMG_'.random_int(11111, 99999).".jpeg";
-            $img_path = UPLOAD_IMAGE_PATH.USERS_FOLDER.$rname;
+            $img_path = $GLOBALS['frontendImagePath'].USERS_FOLDER.$rname;
 
             if($ext == 'png' || $ext == 'PNG') {
                 $img = imagecreatefrompng($image['tmp_name']);
