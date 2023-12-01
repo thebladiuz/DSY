8f3bf6b2c74481423b6a113518c2b91fcc3c35b3
Minh <smgames2099@gmail.com>
2023-12-01 17:05:30 +0700

    Update footer.php

--- Frontend/inc/footer.php
+++ Frontend/inc/footer.php
@@ -167,7 +167,7 @@ function alert(type, msg, position='body') {
         alert('error', 'Incorrect Password!');
       } else {
         // Check if the response contains "success"
-        if (this.responseText === 'success') {
+        if (this.responseText.includes("success")) {
           let response = JSON.parse(this.responseText);
           if (response.session_status) {
               alert('success', "Login successful!");
