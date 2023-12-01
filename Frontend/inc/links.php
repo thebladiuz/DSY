29afb6ca976fbe73f1464679278ed666941d815d
Minh <smgames2099@gmail.com>
2023-12-01 13:30:29 +0700

    update

--- Frontend/inc/links.php
+++ Frontend/inc/links.php
@@ -13,8 +13,6 @@ var_dump($_SESSION);
 
 $sessionStatus = isset($_SESSION['login']) ? $_SESSION['login'] : false;
 
-echo "Session started successfully!"; // Debug statement
-
 require('../Frontend/admin/inc/db_config.php');
 require('../Frontend/admin/inc/essentials.php');
 
