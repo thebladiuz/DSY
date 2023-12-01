f3d49390387688d84cdae5d44cb9444aeb7e7091
Minh <smgames2099@gmail.com>
2023-12-01 13:54:26 +0700

    Update links.php

--- Frontend/inc/links.php
+++ Frontend/inc/links.php
@@ -13,6 +13,8 @@ var_dump($_SESSION);
 
 $sessionStatus = isset($_SESSION['login']) ? $_SESSION['login'] : false;
 
+echo json_encode(['sessionStatus' => $sessionStatus]);
+
 require('../Frontend/admin/inc/db_config.php');
 require('../Frontend/admin/inc/essentials.php');
 
