84c26fd1c4a49c4935969e4b6ad37cfbc58da194
Minh <smgames2099@gmail.com>
2023-12-01 14:39:12 +0700

    update

--- Frontend/admin/index.php
+++ Frontend/admin/index.php
@@ -47,7 +47,7 @@ if((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
     if(isset($_POST['login']))
     {
         $frm_data = filteration($_POST);
-        $query = "SELECT * FROM admin_cred WHERE admin_name=? AND admin_pass=?";
+        $query = "SELECT * FROM admin_cred WHERE BINARY admin_name=? AND BINARY admin_pass=?";
         $values = [$frm_data['admin_name'],$frm_data['admin_pass']];
 
         $res = select($query,$values,"ss");
